f239912b6a501c545f34e3ce42b614c0ed6c65d4
k-sugawa <sugawakumiko@gmail.com>
2024-06-19 23:31:05 +0900

    datajson設定

--- src/components/FormInputAdd.js
+++ src/components/FormInputAdd.js
@@ -1,36 +1,27 @@
 import React, { useState } from "react";
 import searchIcon from "../images/icon-search.svg";
+import data from "../data/data.json"
 
 export const FormInputAdd = ({ onSearchResult }) => {
   const [searchQuery, setSearchQuery] = useState("");
 
-  // const handleSearch = async () => {
-  //   const apiUrl = `https://wordsapiv1.p.rapidapi.com/words/${searchQuery}`;
-  //   const options = {
-  //     method: "GET",
-  //     headers: {
-  //       "X-RapidAPI-Key": "21210d65a1msha43ea28b86b0a5ap190e59jsn2385fb4b68f2",
-  //       "X-RapidAPI-Host": "wordsapiv1.p.rapidapi.com",
-  //     },
-  //   };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (searchQuery.trim() === "") return;
 
-  //   try {
-  //     const response = await fetch(apiUrl, options);
-  //     const result = await response.json();
-  //     onSearchResult(result);
-  //   } catch (error) {
-  //     console.error(error);
-  //   }
-  // };
+    const result = data.find(item => item.word.toLowerCase() === searchQuery.toLowerCase());
+
+    if (result) {
+      onSearchResult(result);
+    } else {
+      onSearchResult({ word: searchQuery, definition: "" });
+    }
+  };
+
 
   return (
     <>
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          //handleSearch();
-        }}
-      >
+      <form onSubmit={handleSubmit}>
         <label>
           <input
             type="text"
